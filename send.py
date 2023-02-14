fea50553bd95916022d7446fe9e734e81be8bf83
SunZhibo <3180105345@zju.edu.cn>
2023-02-14 12:38:52 +0800

    [Dev] add time counter & code swap

---
+++ send.py
@@ -0,0 +1,15 @@
+import socket
+import time
+
+UDP_IP = "192.168.1.1"
+UDP_PORT = 23333
+MESSAGE = "HELLO WORLD"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+
+'''
+compile command
+go env -w GOPROXY=https://goproxy.cn GO111MODULE=on | go run build/ci.go install ./cmd/evm
+'''
+
