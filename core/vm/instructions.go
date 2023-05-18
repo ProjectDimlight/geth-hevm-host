df3cf1af480b7e1507095741d2de1a1e05664d30
SOL <sol.he@my.cityu.edu.hk>
2023-05-18 20:42:26 +0800

    [Dev] Call test

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -700,8 +700,7 @@ func opCall(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([]byt
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
-		ret = common.CopyBytes(ret)
-		scope.Memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
+		interpreter.setMemory(scope, uint32(retOffset.Uint64()), uint32(retSize.Uint64()), ret)
 	}
 	scope.Contract.Gas += returnGas
 
@@ -736,8 +735,7 @@ func opCallCode(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
-		ret = common.CopyBytes(ret)
-		scope.Memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
+		interpreter.setMemory(scope, uint32(retOffset.Uint64()), uint32(retSize.Uint64()), ret)
 	}
 	scope.Contract.Gas += returnGas
 
@@ -765,8 +763,7 @@ func opDelegateCall(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
-		ret = common.CopyBytes(ret)
-		scope.Memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
+		interpreter.setMemory(scope, uint32(retOffset.Uint64()), uint32(retSize.Uint64()), ret)
 	}
 	scope.Contract.Gas += returnGas
 
@@ -794,8 +791,7 @@ func opStaticCall(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) 
 	}
 	stack.push(&temp)
 	if err == nil || err == ErrExecutionReverted {
-		ret = common.CopyBytes(ret)
-		scope.Memory.Set(retOffset.Uint64(), retSize.Uint64(), ret)
+		interpreter.setMemory(scope, uint32(retOffset.Uint64()), uint32(retSize.Uint64()), ret)
 	}
 	scope.Contract.Gas += returnGas
 
